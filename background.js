c2e2f929e66f1ddaa9d4486d85e3ccf770794eba
tkhrmd <maeda1991@gmail.com>
2018-03-22 00:34:04 +0900

    add popup

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.browserAction.onClicked.addListener(function() {
-  chrome.tabs.create({
-    url: 'https://minagine.awg.co.jp/hcm/work/outtimewrkapplymngmnt?do=show_input_new&minazan=1',
-  });
-});
